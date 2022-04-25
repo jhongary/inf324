ef0ad9152bce7d8284174a5b9db9f8021f563b42
jhongary <garymarin6@gmail.com>
2022-04-24 22:26:37 -0400

    pregunta1

---
+++ Nuevo.php
@@ -0,0 +1,86 @@
+
+<html class="no-js" lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Foundation | Welcome</title>
+  <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+</head>
+
+<body>
+
+  <!-- Start Top Bar -->
+  <div class="top-bar">
+    <div class="top-bar-left">
+      <ul class="menu">
+        <li class="menu-text">INF 324</li>
+      </ul>
+    </div>
+    <div class="top-bar-right">
+      <ul class="menu">
+        <li><a href="#">My Account</a></li>
+
+      </ul>
+    </div>
+  </div>
+  <!-- End Top Bar -->
+
+  <div class="callout large primary">
+    <div class="row column text-center">
+      <h1>Pregunta 1</h1>
+    </div>
+  </div>
+
+  <div class="row" id="content">   
+    
+    <div class="medium-8 columns">     
+
+      <div class="blog-post">
+        <div class="callout">
+          <ul class="menu simple">
+            <li><a href="http://www.fcpn.edu.bo/" target="_blank">FACULTAD</a></li>            
+          </ul>
+        </div>
+      </div>
+      <div class="blog-post">
+        <div class="callout">
+          <ul class="menu simple">
+            <li><a href="http://cmat.umsa.bo/" target="_blank">MATEMATICA</a></li>            
+          </ul>
+        </div>
+      </div>
+      <div class="blog-post">
+        <div class="callout">
+          <ul class="menu simple">
+            <li><a href="http://www.fiumsa.edu.bo/" target="_blank">FISICA</a></li>            
+          </ul>
+        </div>
+      </div>  
+      <div class="blog-post">
+        <div class="callout">
+          <ul class="menu simple">
+            <li><a href="http://cienciasquimicas.umsa.bo/" target="_blank">QUIMICA</a></li>            
+          </ul>
+        </div>
+      </div> 
+    </div>
+
+
+    <div class="medium-3 columns  " >
+      <div class="sticky " data-sticky data-anchor="content">
+        <h4>Categories</h4>
+        <ul>
+          <li><a href="#">Skyler</a></li>
+          <li><a href="#">Jesse</a></li>
+          <li><a href="#">Mike</a></li>
+          <li><a href="#">Holly</a></li>
+        </ul>        
+      </div>
+    </div>
+    
+  </div> 
+
+</body>
+
+</html>
